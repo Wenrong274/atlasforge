e4c0cb46bef5bda9864e4a3ec1bc418642f8ed96
Wenrong274 <hybrid0930@gmail.com>
2026-06-16 01:25:46 +0800

    feat: add styles and converters

---
+++ src/AtlasForge/Views/Converters/PackingModeToBoolConverter.cs
@@ -0,0 +1,20 @@
+using System.Globalization;
+using System.Windows.Data;
+using AtlasForge.Models;
+
+namespace AtlasForge.Views.Converters;
+
+public class PackingModeToBoolConverter : IValueConverter
+{
+    public PackingMode TrueValue { get; set; } = PackingMode.Grid;
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+        value is PackingMode mode && mode == TrueValue;
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+        value is true
+            ? TrueValue
+            : TrueValue == PackingMode.Grid
+                ? PackingMode.BinPack
+                : PackingMode.Grid;
+}
